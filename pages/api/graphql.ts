41a5a396f78a81d0d811bba543d7b81c1fffb252
mabuhanifa <moh.abuhanifa@gmail.com>
2023-11-06 00:01:16 +0600

    import fixed

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,6 +1,7 @@
 import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
-import { resolvers, typeDefs } from "/";
+import { resolvers } from "../../graphql/resolvers";
+import { typeDefs } from "../../graphql/schema";
 
 const server = new ApolloServer({
   typeDefs,
